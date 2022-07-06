75f30d9e1d514cd11b579f8c2cb3118502d94fa9
Leah Isenor <leah.isenor@dal.ca>
2022-07-06 17:48:56 -0300

    changed error handling for image processing

--- ec2-client/client/src/utils/imageProcesser.js
+++ ec2-client/client/src/utils/imageProcesser.js
@@ -1,18 +1,16 @@
 import axios from "axios";
 const FormData = require('form-data');
-const url = "http://localhost:5000"
+const url = "http://localhost:5000"//change
+
 export async function processImage(image) {
     const userid = "lisenor"// need to get the active users id
     try {
-    
         await createTable(userid);
         const key = await uploadToBucket(image, userid);
         const text = await getText(userid, key);
         return text;  
-
-    } catch (e) {
+    } catch(e) {
         console.log(e);
-        alert("Error processing image");
     }
 }
 
@@ -40,17 +38,25 @@ async function uploadToBucket(image, userid) {
         return response.data.key;
     }catch(e){
         console.log(e);
+        alert("Error uploading image");
+        throw(e);
     };
 }
 
 async function getText(userid, key) {
-    try{
-        await new Promise(r => setTimeout(r, 8000)); //change this
-
-        var text = "";
-        const response = await axios.get(url+"/text/"+userid+"/"+key);
-        return response.data.text;
-    }catch(e) {
-        console.log(e);
-    };
+    var text = null;
+    for (let tries=0; tries<3 && !text; tries++) {
+        try{
+            await new Promise(r => setTimeout(r, 2000));
+            const response = await axios.get(url+"/text/"+userid+"/"+key);
+            text = response.data.text;
+        }catch(e) {
+            console.log("Could not get text. Attempt: "+tries)
+            console.log(e);
+        }
+    }
+    if (!text) {
+        alert("Error fetching text");
+    }
+    return text;
 }
