1e1bc2367080148ecf955d9bd4625bc0e1c5ec35
Leah Isenor <leah.isenor@dal.ca>
2022-07-07 10:56:47 -0300

    changed server url

--- ec2-client/client/src/utils/imageProcesser.js
+++ ec2-client/client/src/utils/imageProcesser.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const FormData = require('form-data');
-const url = "http://localhost:5000"//change
+const url = "http://group42-dal-2.us-east-1.elasticbeanstalk.com/";
 
 export async function processImage(image) {
     const userid = "lisenor"// need to get the active users id
