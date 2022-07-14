aa3b127b80ab3d5e5a0d755059c9be7b1eccb625
Hemanth Nadipineni <54321hemanth@gmail.com>
2022-07-14 04:30:27 -0300

    cognito configured

--- ec2-client/client/src/App.js
+++ ec2-client/client/src/App.js
@@ -4,18 +4,22 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AuthHub from "./components/authHub";
 import GetUsers from "./components/getUsers";
 import FileUpload from "./components/fileUpload";
+import AppRoutes from "./Routes";
+
+
 
 import "./App.css";
 
 function App() {
     return (
         <div className="App">  
-            <BrowserRouter>
+        <AppRoutes />
+            {/* <BrowserRouter>
                 <Routes>
                     <Route path="/uploadtest" element={<FileUpload />}/>
                     <Route path="/" element={<div><AuthHub /><GetUsers /></div>}/>
                 </Routes>
-            </BrowserRouter>
+            </BrowserRouter> */}
         </div>
     );
 }
